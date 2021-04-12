aa39521b24583ee11903839a8de568e6ec1ef64d
Charlesiubao <kristoffer.charles.reyes@gmail.com>
2021-04-12 13:04:59 -0700

    again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const patientRoutes = require('./routes/patientRoutes')
 app.use(express.json())
 app.use(require('cors')())
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`port running on ${PORT}`)
   routesReport.print()
